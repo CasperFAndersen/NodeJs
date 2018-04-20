5cef9bd2dd9cf60e154a1439c6833a1c96ee6ad6
Casper Froberg Andersen <casperfroberg@gmail.com>
2018-04-20 10:07:08 +0200

    mongodb tests.

---
+++ Lektion 7/MongoDbTest/app.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://nodeuser:nodeuser123@ds247439.mlab.com:47439/testdbcasper');
+
+var Schema = mongoose.Schema;
+var bookSchema = new Schema({
+    title: String,
+    author: String,
+    Year: String
+});
+var Book = mongoose.model('Book', bookSchema);
+var harryPotter = Book({
+    title: "C#",
+    author: "Troelsen",
+    year: 2016
+})
+harryPotter.save(function (err) {
+    if (err) console.log(err);
+    console.log('succeded!');
+})
+
+Book.find({}), function (err, book) {
+    if (err) console.log(err);
+    console.log(books);
+}
