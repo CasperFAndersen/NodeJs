2460da1d525672d1c8146aba032e31c0b0eb39f9
Casper Froberg Andersen <casperfroberg@gmail.com>
2018-07-05 11:50:35 +0200

    uncommented not woring code.

--- Lektion 5/Node-Ejs-Template-exercise/controllers/workController/work.js
+++ Lektion 5/Node-Ejs-Template-exercise/controllers/workController/work.js
@@ -2,15 +2,15 @@ const express = require("express");
 const router = express.Router();
 const fs = require('fs')
 
-exports.numberOfHoursWorked(req, res, next) => {
-    console.log("number of hours worked calculation endpoint hit");
+// exports.numberOfHoursWorked(req, res, next) => {
+//     console.log("number of hours worked calculation endpoint hit");
 
-    const startTime = req.body.startTime;
-    const endTime = req.body.endTime;
+//     const startTime = req.body.startTime;
+//     const endTime = req.body.endTime;
 
-    let workedTime = endTime - startTime;
+//     let workedTime = endTime - startTime;
 
-    res.render('workViews', {
-        msg: "worked time = " + workedTime
-    });
-}
+//     res.render('workViews', {
+//         msg: "worked time = " + workedTime
+//     });
+// }
